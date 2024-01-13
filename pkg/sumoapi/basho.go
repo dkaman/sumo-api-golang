1829ac6b7770963b25b49e1b9c3392190703c43e
Dallas Kaman <dallas.kaman@gmail.com>
2024-01-12 21:11:08 -0600

    initial basho types

--- pkg/sumoapi/basho.go
+++ pkg/sumoapi/basho.go
@@ -1,3 +1,27 @@
 package sumoapi
 
+import "time"
+
 type BashoService service
+
+type prizeEntry struct {
+	Type      string `json:"type,omitempty"`
+	RikishiID int    `json:"rikishiId,omitempty"`
+	Name      string `json:"shikonaEn,omitempty"`
+	//Shikona string `json:"shikonaJp,omitempty"`
+}
+
+type Basho struct {
+	Date          string       `json:"date,omitempty"`
+	Location      string       `json:"location,omitempty"`
+	StartDate     time.Time    `json:"startDate,omitempty"`
+	EndDate       time.Time    `json:"endDate,omitempty"`
+	Yusho         []prizeEntry `json:"yusho,omitempty"`
+	SpecialPrizes []prizeEntry `json:"specialPrizes,omitempty"`
+}
+
+type Torikumi struct {
+	Basho
+
+	Torikumi []Match `json:"torikumi,omitempty"`
+}
